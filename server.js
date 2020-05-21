f0f00d86654433a084711cc5f386a39ccdc3e668
David noh <dogyunnoh@gmail.com>
2020-05-21 18:07:44 +0900

    5.21 nodejs

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var express = require('express')
 var path = require('path')
-var badyParser = require('body-parser')
+var bodyParser = require('body-parser')
+var apiRouter = require('./routes/apiRouter')
+//require('./routes/apiRouter')
 var app = express()
+
 
 app.set('views', path.join(__dirname+'/views'))
 app.set('view engine', 'ejs')
 
-//app.use() 미들웨어
 
-app.get('/main' ,function(request,response){
-console.log(request)
-response.render('index')
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/api',apiRouter )
+app.use('/',(req,res){
+console.log(request.body)
+response.render("index")
+}
+//app.get은 get방식으로 라우팅 처리 한 것 이다 (중계역할)
+// app.get('/main' ,function(request,response){
+// console.log(request)
+// response.render('index')
+// })
 // listen(포트, url , backlog , callback)
 app.listen(8080 , function(){
 console.log("server is starting at http://localhost:8080")
